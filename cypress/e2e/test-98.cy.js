225cdd7ff370a4e1b6146750191120a7dd8c7c62
Jose Barranco <90844270+barrancojose@users.noreply.github.com>
2025-09-17 15:33:30 -0300

    chore: update generated Cypress tests

--- cypress/e2e/test-98.cy.js
+++ cypress/e2e/test-98.cy.js
@@ -1,5 +1,12 @@
-describe('Feature: Login', () => {
-  it('Scenario: Successful login', () => {
+describe('Feature: Login as Admin', () => {
+  it('Scenario: Successful login as Admin', () => {
+    cy.visit('http://localhost:3000');
+    
+      cy.get('input[name="username"]').type('admin');
+      cy.get('input[name="password"]').type('1234');
+      cy.contains('button','Login').click();
+    
+    cy.contains('Testing Platform');
     cy.visit('http://localhost:3000');
     
       cy.get('input[name="username"]').type('admin');
