eba52f8e0677debe79ac624ec7203e483ab7408a
Jose Barranco <fbarranco@slingr.io>
2025-09-19 16:51:55 -0300

    Changes in Cypress configuration

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,15 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
+      return config;
     },
-  },
+    reporter: "cypress-mochawesome-reporter",
+    reporterOptions: {
+      reportDir: "cypress/reports/json",
+      overwrite: false,
+      html: false,
+      json: true
+    }
+  }
 });
