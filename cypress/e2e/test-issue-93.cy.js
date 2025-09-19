bfa4f6b852b31a53fe16d4609ca7357e013081c5
Jose Barranco <90844270+barrancojose@users.noreply.github.com>
2025-09-19 16:00:15 -0300

    chore: update generated Cypress tests

--- cypress/e2e/test-issue-93.cy.js
+++ cypress/e2e/test-issue-93.cy.js
@@ -1,5 +1,5 @@
 describe('Feature: Login', () => {
-  it('Scenario: Successful login', () => {
+  it('Scenario: Successful login as Admin', () => {
     cy.visit('https://testingplatform.tarzan.io/dev/runtime/');
     
       cy.get('input[name="username"]').type('admin');
@@ -11,10 +11,10 @@ describe('Feature: Login', () => {
 });
 
 describe('Feature: Login as Admin', () => {
-  it('Scenario: Successful login', () => {
+  it('Scenario: Successful login as user', () => {
     cy.visit('https://testingplatform.tarzan.io/dev/runtime/');
     
-      cy.get('input[name="username"]').type('admin');
+      cy.get('input[name="username"]').type('userQA');
       cy.get('input[name="password"]').type('1234');
       cy.contains('button','Login').click();
     
