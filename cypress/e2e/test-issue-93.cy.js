75698e260ce40ffe33cbbcd585a56326006b29e0
Jose Barranco <90844270+barrancojose@users.noreply.github.com>
2025-09-19 14:58:20 -0300

    chore: create generated Cypress tests

---
+++ cypress/e2e/test-issue-93.cy.js
@@ -0,0 +1,24 @@
+describe('Feature: Login', () => {
+  it('Scenario: Successful login', () => {
+    cy.visit('https://testingplatform.tarzan.io/dev/runtime/');
+    
+      cy.get('input[name="username"]').type('admin');
+      cy.get('input[name="password"]').type('1234');
+      cy.contains('button','Login').click();
+    
+    cy.contains('Testing Platform');
+  });
+});
+
+describe('Feature: Login as Admin', () => {
+  it('Scenario: Successful login', () => {
+    cy.visit('https://testingplatform.tarzan.io/dev/runtime/');
+    
+      cy.get('input[name="username"]').type('admin');
+      cy.get('input[name="password"]').type('1234');
+      cy.contains('button','Login').click();
+    
+    cy.contains('Testing Platform');
+  });
+});
+
